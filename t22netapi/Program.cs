55a6c9eec71d258507e4119e9af1e5f5bf57bbe1
UngSyMui <ungsymui@gmail.com>
2023-05-26 20:14:30 +0700

    exam

--- t22netapi/Program.cs
+++ t22netapi/Program.cs
@@ -20,13 +20,13 @@ builder.Services.AddCors(options =>
 );
 
 
-//builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//var connectionString = builder.Configuration.GetConnectionString("t22api");
-//builder.Services.AddDbContext<t22netapi.Entities.T22netContext>(
-//    options => options.UseSqlServer(connectionString));
+builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("t23api");
+builder.Services.AddDbContext<t22netapi.Entities.ThiThucHanhContext>(
+    options => options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
