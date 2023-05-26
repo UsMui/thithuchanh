fafedc290e08b23235fec254cdc91b58b747404a
UngSyMui <ungsymui@gmail.com>
2023-05-26 20:16:13 +0700

    exam

--- t22netapi/Program.cs
+++ t22netapi/Program.cs
@@ -3,22 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//Add cors
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            //policy.WithOrigins("https://24h.com.vn");
-            policy.AllowAnyOrigin();
-            policy.AllowAnyMethod();
-            policy.AllowAnyHeader();
-        }
-        );
-}
-);
-
 
 builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
